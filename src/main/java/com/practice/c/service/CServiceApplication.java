98e54f56ac401d6508d9d7f05c18366fb820540e
TrucXNguyen <nguyenxuantruc30111997@gmail.com>
2022-08-08 15:45:52 +0700

    [TrucNguyen] init

---
+++ src/main/java/com/practice/c/service/CServiceApplication.java
@@ -0,0 +1,13 @@
+package com.practice.c.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/c/service/CServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.c.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
